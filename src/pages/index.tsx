85983553c7caaa30835bb3a0de4cd68c54bd3770
zym <917074403@qq.com>
2021-05-14 19:44:14 +0800

    简化主页面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,6 @@ export default function IndexPage() {
   return (
     <div className={styles.container}>
       <p>Hello Umi</p>
-      <DatePicker />
-      <Button type="primary" style={{ marginLeft: 8 }}>
-        Primary Button
-      </Button>
       <div>
         <Link to="/about">goto about</Link>
       </div>
